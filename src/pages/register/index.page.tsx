d516553ea4f99ef4bd828e552b9cd09885c3b1a4
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2023-11-02 15:14:02 -0400

    add: username input default value based on query param from the url

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { FormError } from "../home/components/ClaimUsernameForm/style";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const registerFormSchema = z.object({
   username: z
@@ -25,10 +27,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue("username", String(router.query.username));
+    }
+  }, [router.query?.username, setValue]);
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data);
