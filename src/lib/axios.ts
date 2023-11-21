6ee8d9e201996a755264962539cf518ebac84b4e
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2023-11-21 19:50:41 -0400

    add: cors

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "/api",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
