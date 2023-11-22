dec4e74711d0c9195528c33a58edf5848ff9edf6
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2023-11-22 18:27:39 -0400

    removed cors

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "/api",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Access-Control-Allow-Origin": "*",
-  },
 });
